74cdcacd6ce2852d9caa6b4741e346f6a914df38
pavan <pavankumarsai18@gmail.com>
2020-03-29 15:35:34 -0400

    Made a temp main file.

--- main.java
+++ main.java
@@ -1,1 +1,24 @@
 //this is where we will have main
+
+import java.awt.*;
+import javax.swing.*;
+
+import javax.swing.JFrame;
+
+class main 
+//extends JPanel implements ActionListner
+{
+	
+	public static void main(String args[])
+	{
+		JFrame frame = new JFrame("Chess");
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1000, 1294);
+		
+		
+		return;
+	}
+	
+}
