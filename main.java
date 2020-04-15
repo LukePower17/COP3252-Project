dc043c35a22530e68a38ca7161c63fa165677255
LukePower17 <62807171+LukePower17@users.noreply.github.com>
2020-04-15 18:48:09 -0400

    Add files via upload
    added in the icons for pieces

--- main.java
+++ main.java
@@ -1,24 +1,102 @@
-//this is where we will have main
-
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.Container;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
 import javax.swing.*;
 
-import javax.swing.JFrame;
-
-class main 
+//import Board;
+public class main extends JFrame
 //extends JPanel implements ActionListner
 {
-	
+
+
 	public static void main(String args[])
 	{
 		JFrame frame = new JFrame("Chess");
-		
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1000, 1294);
+		frame.setSize(800, 800);
+		NewBoard Board = new NewBoard();
+		JPanel controls = new JPanel();
+		controls.setLayout(new GridLayout(8, 8,0,0));
 		
-		
+		Icon WPawn = new ImageIcon("white_pawn.png");
+		Icon BPawn = new ImageIcon("black_pawn.png");
+		Icon WBish = new ImageIcon("white_bishop.png");
+		Icon BBish = new ImageIcon("black_bishop.png");
+		Icon WRook = new ImageIcon("white_rook.png");
+		Icon BRook = new ImageIcon("black_rook.png");
+		Icon WQueen = new ImageIcon("white_queen.png");
+		Icon BQueen = new ImageIcon("black_queen.png");
+		Icon BKing = new ImageIcon("black_king.png");
+		Icon WKing = new ImageIcon("white_king.png");
+		Icon BKnight = new ImageIcon("black_knight.png");
+		Icon WKnight = new ImageIcon("white_knight.png");
+
+		for(int i = 0; i < 8; i++)//i is rows
+		{
+			for(int j = 0; j < 8; j++)
+			{
+				//Rectangle sq = new Rectangle(x, y, 100, 100);
+
+				JButton b = new JButton();
+				if(i == 1)
+				{
+
+    			b.setIcon(WPawn);
+				}
+
+				if(j%2 == (i%2)){
+					b.setBackground(new Color(118, 150, 86));
+
+				}
+				else
+				{
+					b.setBackground(new Color(238, 238, 210));
+
+				}
+				b.setBorder(null);
+				b.setOpaque(true);
+
+				controls.add(b);
+			}
+
+		}
+
+
+		frame.add(controls);
 		return;
 	}
-	
+	// public void paintComponent(Graphics g) {
+	//    g.setColor(Color.GREEN);
+	//    g.fillRect(0, 0, getSize().width, getSize().height);
+	// }
+
 }
+
+
+
+// public class Board extends JComponent
+// {
+// 	public void paintComponent(Graphics g)
+// 	{
+// 		super.paintComponent(g);
+// 		Graphics2D g2d = (Graphics2D)g;
+//
+// 		int startX = 50;
+// 		int startY = 0;
+// 		Rectangle board = new Rectangle(startX,startY, 800,800);
+// 		g2d.draw(board);
+//
+// 		int x = startX;
+// 		int y = startY;
+//
+// 		int sideLen = 80;
+//
+//
+// 	}
+// }
